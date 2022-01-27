2095654d0bac129bf53d57d3826f62e1d9be9d9b
Khalid Mesbah <89298608+khalidMesbah@users.noreply.github.com>
2022-01-27 17:39:34 +0200

    Update is the matrix diagonal.cpp

--- is the matrix diagonal.cpp
+++ is the matrix diagonal.cpp
@@ -1,5 +1,5 @@
 /*
-the square matrix (D) is diagonal if 
+the square matrix (D) is diagonal if
 Dij = 0 ∀ i != j
 */
 
@@ -8,28 +8,24 @@ using namespace std;
 
 int main()
 {
-    // int arr[3][3] = {
-    //     {1, 2, 3},
-    //     {4, 5, 6},
-    //     {7, 8, 9}
-    // };
+    int n;
+    cout << "enter the number of rows/cols of the square matrix\n";
+    cin >> n;
 
-    int arr[3][3] = {
-        {1, 0, 0},
-        {0, 5, 0},
-        {0, 0, 9}
-    };
-
+    int arr[n][n]{0};
     int is_diagonal = 1;
-    for (int i = 0; i < 3; i++)
-        for (int j = 0; j < 3; j++)
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+        {
+            cin >> arr[i][j];
             if (i != j && arr[i][j] != 0)
                 is_diagonal = 0;
+        }
 
     if (is_diagonal)
         cout << "the array is daigonal";
     else
         cout << "the array is NOT daigonal";
-        
+
     return 0;
 }
