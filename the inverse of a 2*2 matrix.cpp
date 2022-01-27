f168f59cca7e12222f17a9fbdd3b878a5c485a87
Khalid Mesbah <89298608+khalidMesbah@users.noreply.github.com>
2022-01-27 15:56:24 +0200

    Update and rename the inverse of a 2*2 matrix-problem.cpp to the inverse of a 2*2 matrix.cpp

--- the inverse of a 2*2 matrix.cpp
+++ the inverse of a 2*2 matrix.cpp
@@ -3,17 +3,18 @@ using namespace std;
 
 int main()
 {
-    long arr[2][2] = {
-        {1, 2},
-        {4, 5}};
+    double arr[2][2];
+    for (int i = 0; i < 2; i++)
+        for (int j = 0; j < 2; j++)
+            cin >> arr[i][j];
 
-    long temp;
-    long detArr = 1 / (arr[1][1] * arr[0][0] - arr[0][1] * arr[1][0]);
+    double temp;
+    double detArr = 1 / (arr[1][1] * arr[0][0] - arr[0][1] * arr[1][0]);
     arr[0][1] *= -1;
     arr[1][0] *= -1;
-    temp = arr[1][1];
-    arr[1][1] = arr[2][2];
-    arr[2][2] = temp;
+    temp = arr[0][0];
+    arr[0][0] = arr[1][1];
+    arr[1][1] = temp;
 
     for (int i = 0; i < 2; i++)
         for (int j = 0; j < 2; j++)
