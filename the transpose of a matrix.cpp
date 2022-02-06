2f6d36063e5356e37306c73a67a28526971db9a4
Khalid Mesbah <89298608+khalidMesbah@users.noreply.github.com>
2022-02-06 14:35:40 +0200

    Update and rename the transpose of a matrix-problem.cpp to the transpose of a matrix.cpp

---
+++ the transpose of a matrix.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+     cout << "Enter the dimensions of the matrix: ";
+     int n, m;
+     cin >> n >> m;
+     int arr[n][m], trans_arr[m][n];
+
+     for (int i = 0; i < n; i++)
+          for (int j = 0; j < m; j++)
+               cin >> arr[i][j];
+
+     for (int i = 0; i < m; i++)
+          for (int j = 0; j < n; j++)
+               trans_arr[i][j] = arr[j][i];
+
+     cout << "the transpose of the matrix is:-\n";
+     for (int i = 0; i < m; i++)
+     {
+          for (int j = 0; j < n; j++)
+               cout << trans_arr[i][j] << " ";
+          cout << endl;
+     }
+
+     return 0;
+}
